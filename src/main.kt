d022890d725a5882282d7613d9a09dcd84013869
maditis <manik6418@gmail.com>
2023-12-09 09:02:49 -0800

    day 1 part 2

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,40 @@
 import java.io.File
 import java.io.FileNotFoundException
 
+
+val digits_map: Map<String, Char> = mapOf(
+    "zero" to '0',
+    "one" to '1',
+    "two" to '2',
+    "three" to '3',
+    "four" to '4',
+    "five" to '5',
+    "six" to '6',
+    "seven" to '7',
+    "eight" to '8',
+    "nine" to '9',
+    )
 fun main() {
     var file: File = File("input.txt")
-    var sum_values: Int = 0
+
+    // immutable map, immutable variable
 
     try {
-        file.forEachLine(
+        var sum_values = process_lines(file=file)
+        println(sum_values)
+
+    } catch (e: FileNotFoundException) {
+        println("Could not read the file: Provide correct Path")
+    } catch(e: Exception) {
+        println("Programming Error ${e}")
+    }
+
+}
+
+
+fun process_lines(file: File): Int {
+    var sum_values: Int = 0
+    file.forEachLine(
                 action = { line: String ->
                     // loop through each line
                     // check if its number
@@ -14,12 +42,26 @@ fun main() {
                     // intialize start and end pointer
                     var start: Char? = null
                     var end: Char? = null
-                    for (char in line) {
-                        if (char.isDigit()) {
+                    // check if substring is a digit
+                    for (item in digits_map.entries) {
+
+                        if (item.key in line.lowercase()) {
                             if (start == null) {
-                                start = char
+                                start = item.value.toChar()
                             } else {
-                                end = char
+                                end = item.value.toChar()
+                            }
+                        }
+                        val index = line.lowercase().indexOf(item.key) + item.key.length - 1
+                        for (char in line) {
+                            if (char.isDigit()) {
+                                val index2 = line.indexOf(char)
+
+                                if (start == null) {
+                                    start = char
+                                } else if (index2 > index) {
+                                    end = char
+                                }
                             }
                         }
                     }
@@ -35,11 +77,6 @@ fun main() {
                     // println(number)
 
                     sum_values += number.toInt()
-                }
-        )
-    } catch (e: FileNotFoundException) {
-        println("Could not read the file: Provide correct Path")
-    }
-
-    println(sum_values)
+                })
+    return sum_values
 }
